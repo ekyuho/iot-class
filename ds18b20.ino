7120237991974ee73bac9859e5d3107a78c4caa6
Kyuho Kim <ekyuho@gmail.com>
2021-09-15 22:37:25 +0900

    Update ds18b20.ino

--- ds18b20.ino
+++ ds18b20.ino
@@ -8,7 +8,9 @@ DallasTemperature sensors(&oneWire);
 void setup() {
   Serial.begin(115200);
   delay(1000);
+  Serial.println();
   sensors.begin();
+  Serial.println("Begin");
 }
  
 void loop() {
@@ -16,6 +18,4 @@ void loop() {
   delay(1000);
   float tempC = sensors.getTempCByIndex(0);
   Serial.printf("temp=%.1f\n", tempC);
-
-  delay(5000);
 }
