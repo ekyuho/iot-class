829d70af630e184cca30f3c6658912cc4ddb225c
Kyuho Kim <ekyuho@gmail.com>
2022-03-16 11:48:14 +0900

    Update google.py

--- google.py
+++ google.py
@@ -1,16 +1,16 @@
 from socket import *
 
-ip = "www.google.com"
+ip = "damoa.io"
 port = 80
-path = "/"
+path = "/ip"
 
 clientSocket = socket(AF_INET, SOCK_STREAM)	
 clientSocket.connect((ip,port))			
 
 print("연결 확인됐습니다.")
-clientSocket.send("GET "+ path + " HTTP/1.1\n\n".encode("utf-8"))		
+clientSocket.send(("GET "+ path +" HTTP/1.1\r\n\r\n").encode("utf-8"))		
 
 data = clientSocket.recv(10240)					
 
 print("받은 데이터 : ",data.decode("utf-8"))
-clientSocket.close()						
+clientSocket.close()		
