f1f55831a92be6047369091ac83af11ddaf010f6
Kyuho Kim <ekyuho@gmail.com>
2022-03-16 11:40:32 +0900

    Update google.py

--- google.py
+++ google.py
@@ -2,12 +2,13 @@ from socket import *
 
 ip = "www.google.com"
 port = 80
+path = "/"
 
 clientSocket = socket(AF_INET, SOCK_STREAM)	
 clientSocket.connect((ip,port))			
 
 print("연결 확인됐습니다.")
-clientSocket.send("GET / HTTP/1.1\n\n".encode("utf-8"))		
+clientSocket.send("GET "+ path + " HTTP/1.1\n\n".encode("utf-8"))		
 
 data = clientSocket.recv(10240)					
 
